43bbaf8def7e1c0fd59be79278adca665ccfe711
tauisf-jawaid <tausifjawaid6@gmail.com>
2026-05-09 21:20:25 +0530

    Initial project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => {
+        console.error("Error fetching message", err);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome to chaicode frontend</h1>
+      <h2>{message}</h2>
+    </>
+  );
+}
+
+export default App;
